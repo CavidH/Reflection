09e897401ac2c79d42340cc244287ee3cb5a8f9c
Cavid <hcavid386@gmail.com>
2022-12-13 13:22:11 -0800

    Initial commit

---
+++ Reflection/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Reflection.Models;
+
+public class User
+{
+    public string UserName { get; set; }
+    public string Email { get; set; }
+
+    public string GetValue(string propName) => this.GetType().GetProperty(propName).GetValue(this, null).ToString();
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,12 @@
+﻿using Reflection.Models;
+
+User t = new User()
+{
+    UserName ="Cavid" ,
+    Email = "HCavid386@gmail.com"
+};
+
+// t.UserName = "Cavid";
+// t.Email = "HCavid386@gmail.com";
+
+Console.WriteLine("Username: " + t.GetValue("UserName") + "\nEmail: " + t.GetValue("Email"));
