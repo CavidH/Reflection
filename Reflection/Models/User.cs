15ba8cbf26e116fa3304d873d4bd7a669b217d51
Cavid <hcavid386@gmail.com>
2022-12-13 13:24:10 -0800

    fixed

--- Reflection/Models/User.cs
+++ Reflection/Models/User.cs
@@ -5,5 +5,15 @@ public class User
     public string UserName { get; set; }
     public string Email { get; set; }
 
-    public string GetValue(string propName) => this.GetType().GetProperty(propName).GetValue(this, null).ToString();
+    public string GetValue(string propName)
+    {
+        try
+        {
+            return this.GetType().GetProperty(propName).GetValue(this, null).ToString();
+        }
+        catch
+        {
+            return "This property does not exist";
+        }
+    }
 }
